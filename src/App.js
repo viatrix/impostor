47567b5b51a90ad4f351bf7653e5adb56de324c4
viatrix <viatrix.x@gmail.com>
2017-03-14 08:24:40 +0200

    Prototype of questionnaire form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import questions from './questions.json';
+
+let answersText = ["совсем не подходит",
+                "не очень подходит",
+                "нейтрально",
+                "в основном подходит",
+                "полностью подходит"];
 
 class App extends Component {
+    constructor(props) {
+       super(props);
+       this.state = {answers: [], answerSelected: "0"};
+       this.handleClick = this.handleClick.bind(this);
+     }
+
+     handleClick() {
+         var answersStore = this.state.answers;
+         answersStore.push({id: "a1", answer: this.state.answerSelected});
+         this.setState({answers: answersStore});
+         console.log(this.state.answers[this.state.answers.length-1]);
+    }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          Выбирайте наиболее близкий вам вариант ответа
+          <h2>Вопрос</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+           {questions[1].text}
+           <label><input name="answer" type="radio" value = "0"
+                    checked={this.state.answerSelected === "0"}
+                    onChange={(event) => {this.setState({answerSelected: event.target.value})
+                    }}/>{answersText[0]}
+            </label>
+            <label><input name="answer" type="radio" value = "1"
+                     checked={this.state.answerSelected === "1"}
+                     onChange={(event) => {this.setState({answerSelected: event.target.value})
+                 }}/>{answersText[1]}
+             </label>
+             <label><input name="answer" type="radio" value = "2"
+                      checked={this.state.answerSelected === "2"}
+                      onChange={(event) => {this.setState({answerSelected: event.target.value})
+                  }}/>{answersText[2]}
+              </label>
+              <label><input name="answer" type="radio" value = "3"
+                       checked={this.state.answerSelected === "3"}
+                       onChange={(event) => {this.setState({answerSelected: event.target.value})
+                   }}/>{answersText[3]}
+               </label>
+               <label><input name="answer" type="radio" value = "4"
+                        checked={this.state.answerSelected === "4"}
+                        onChange={(event) => {this.setState({answerSelected: event.target.value})
+                    }}/>{answersText[4]}
+                </label>
+           <input id="answerButton" type="button" value="Ответить" onClick={this.handleClick}/>
         </p>
       </div>
     );
   }
 }
 
+
 export default App;
+
