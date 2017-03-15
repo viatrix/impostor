503dd9fedf35c642159f42e844c0d67772548489
viatrix <viatrix.x@gmail.com>
2017-03-15 21:19:52 +0200

    Created working prototype of the test app. Added display of results

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import questions from './questions.json';
+import questionsImport from './questions.json';
+import resultsImport from './results.json';
 
 let answersText = ["совсем не подходит",
                 "не очень подходит",
@@ -11,15 +12,37 @@ let answersText = ["совсем не подходит",
 class App extends Component {
     constructor(props) {
        super(props);
-       this.state = {answers: [], answerSelected: "0"};
+       this.state = {answers: [],
+                    answerSelected: "0",
+                    questionId: 0,
+                    questions: questionsImport,
+                    result: "",
+                    buttonIsDisabled: false};
        this.handleClick = this.handleClick.bind(this);
+       this.updateResult = this.updateResult.bind(this);
+     }
+
+     updateResult() {
+         var res=this.state.result;
+         var lastQuestionId = this.state.answers[this.state.answers.length-1].id;
+         var lastAnswerId = this.state.answers[this.state.answers.length-1].answer;
+         console.log(lastQuestionId, resultsImport[0], resultsImport[lastQuestionId]);
+         res += (" "+resultsImport[lastQuestionId][lastAnswerId]);
+         this.setState({result: res});
      }
 
      handleClick() {
          var answersStore = this.state.answers;
-         answersStore.push({id: "a1", answer: this.state.answerSelected});
+         answersStore.push({id: this.state.questions[this.state.questionId].id, answer: this.state.answerSelected});
          this.setState({answers: answersStore});
-         console.log(this.state.answers[this.state.answers.length-1]);
+         if (this.state.questionId < this.state.questions.length-1){
+             this.setState({questionId: this.state.questionId + 1});
+        }
+        else {
+            this.setState({buttonIsDisabled: true});
+        }
+         console.log(this.state.answers[this.state.answers.length-1], this.state.questionId);
+         this.updateResult();
     }
 
   render() {
@@ -29,35 +52,38 @@ class App extends Component {
           Выбирайте наиболее близкий вам вариант ответа
           <h2>Вопрос</h2>
         </div>
-        <p className="App-intro">
-           {questions[1].text}
-           <label><input name="answer" type="radio" value = "0"
-                    checked={this.state.answerSelected === "0"}
+        <div className="App-intro">
+            <div id="questionPlace">
+                {this.state.questions[this.state.questionId].text}
+           </div>
+           <label><input name="answer" type="radio" value="0"
+                    checked={this.state.answerSelected==="0"}
                     onChange={(event) => {this.setState({answerSelected: event.target.value})
                     }}/>{answersText[0]}
             </label>
-            <label><input name="answer" type="radio" value = "1"
-                     checked={this.state.answerSelected === "1"}
+            <label><input name="answer" type="radio" value="1"
+                     checked={this.state.answerSelected==="1"}
                      onChange={(event) => {this.setState({answerSelected: event.target.value})
                  }}/>{answersText[1]}
              </label>
-             <label><input name="answer" type="radio" value = "2"
-                      checked={this.state.answerSelected === "2"}
+             <label><input name="answer" type="radio" value="2"
+                      checked={this.state.answerSelected==="2"}
                       onChange={(event) => {this.setState({answerSelected: event.target.value})
                   }}/>{answersText[2]}
               </label>
-              <label><input name="answer" type="radio" value = "3"
-                       checked={this.state.answerSelected === "3"}
+              <label><input name="answer" type="radio" value="3"
+                       checked={this.state.answerSelected==="3"}
                        onChange={(event) => {this.setState({answerSelected: event.target.value})
                    }}/>{answersText[3]}
                </label>
-               <label><input name="answer" type="radio" value = "4"
-                        checked={this.state.answerSelected === "4"}
+               <label><input name="answer" type="radio" value="4"
+                        checked={this.state.answerSelected==="4"}
                         onChange={(event) => {this.setState({answerSelected: event.target.value})
                     }}/>{answersText[4]}
                 </label>
-           <input id="answerButton" type="button" value="Ответить" onClick={this.handleClick}/>
-        </p>
+           <input id="answerButton" type="button" value="Ответить" onClick={this.handleClick} disabled={this.state.buttonIsDisabled}/>
+           <p>{this.state.result}</p>
+        </div>
       </div>
     );
   }
@@ -65,4 +91,3 @@ class App extends Component {
 
 
 export default App;
-
